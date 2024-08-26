08dae7b40e068b26788b4ab7218622bc8dc2e40b
Amin Talebi <talebi242@gmail.com>
2024-08-26 22:42:29 +0200

    add account script

--- dextrader/hardhat.config.js
+++ dextrader/hardhat.config.js
@@ -1,11 +1,35 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ARBISCAN_KEY = process.env.ARBISCAN_KEY;
+
 module.exports = {
   solidity: {
     version: "0.8.17",
     settings: {
       optimizer: { enabled: true, runs: 200 },
+    },
+  },
+  etherscan: {
+    apiKey: {
+      arb: ARBISCAN_KEY,
+    },
+    customChains: [
+      {
+        network: "arb",
+        chainId: 42161,
+        urls: {
+          apiURL: "https://api.arbiscan.io/api",
+          browserURL: "https://arbiscan.io",
+        },
+      },
+    ],
+  },
+  networks: {
+    arb: {
+      url: "https://1rpc.io/arb",
+      chainId: 42161,
+      accounts: [PRIVATE_KEY],
     },
   },
 };
